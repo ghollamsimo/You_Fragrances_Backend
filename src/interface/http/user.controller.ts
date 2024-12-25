e33c418822cbaf7319b98bd7b43ca11cef76d96f
ghollamsimo <ghollamsimo1@gmail.com>
2024-12-25 15:06:26 +0100

    Feat --> adding method of register with login

--- src/interface/http/user.controller.ts
+++ src/interface/http/user.controller.ts
@@ -1,13 +1,25 @@
-import {Body, Controller, Post} from "@nestjs/common";
-import {UserUseCase} from "../../application/usecases/user.usecase";
-import {User} from "../../core/entities/user.entity";
+import { Body, Controller, Post } from "@nestjs/common";
+import { UserUseCase } from "../../application/usecases/user.usecase";
+import { UserEntity } from "../../core/entities/user.entity";
+import { RegisterDto } from "src/core/dto/register.dto";
+import { LoginDto } from "src/core/dto/login.dto";
+import { emit } from "process";
 
 @Controller('users')
 export class UserController {
-    constructor(private readonly UserUseCase: UserUseCase) {}
+    constructor(private readonly userUseCase: UserUseCase) {}
 
-    @Post()
-    async store(@Body() body: {name: string; email: string; password: string, gender: string, phone: number}): Promise<User>{
-        return await this.UserUseCase.execute(body.name, body.email, body.password, body.gender, body.phone)
+    @Post('/store')
+    async store(@Body() body: RegisterDto): Promise<UserEntity> {
+        return await this.userUseCase.execute(body);
     }
+
+    @Post("login")
+    async login(
+      @Body() body: { email: string; password: string }
+    ): Promise<{ token: string }> {
+      const loginDto = new LoginDto(body.email, body.password);
+      return this.userUseCase.login(loginDto);
+    }
+  
 }
