2b1a8c29bcb005e8e517df95fe98035c50dfe380
ghollamsimo <ghollamsimo1@gmail.com>
2024-12-25 15:03:23 +0100

    Feat --> Adding method of login and update and then delete

--- src/core/interfaces/user.interface.ts
+++ src/core/interfaces/user.interface.ts
@@ -1,7 +1,11 @@
-import {User} from '../entities/user.entity'
+import {UserEntity} from '../entities/user.entity'
+import { RegisterDto } from '../dto/register.dto';
+import { LoginDto } from '../dto/login.dto';
 
 export interface UserInterface {
-
-    store(user: User): Promise<User>
-    index(user: User): User
+    store(user: RegisterDto): Promise<UserEntity>
+    login(loginDto: LoginDto): Promise<{token: string}>
+    index(): Promise<UserEntity[]>
+    update(id:string, userDto: RegisterDto): Promise<{message: string}>
+    delete(id:string): Promise<{ message: string }>
 }
