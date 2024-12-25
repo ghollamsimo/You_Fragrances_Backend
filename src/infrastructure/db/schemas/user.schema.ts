2e62fa8d141b592c59868f9c17e3315d8928b1d6
ghollamsimo <ghollamsimo1@gmail.com>
2024-12-25 14:59:09 +0100

    Feat --> Adding field of gender with role (enum) and removing field of phone

--- src/infrastructure/db/schemas/user.schema.ts
+++ src/infrastructure/db/schemas/user.schema.ts
@@ -1,22 +1,29 @@
 import { Document } from 'mongoose';
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 
+export enum UserRole {
+  ADMIN = 'admin',
+  CLIENT = 'client',
+}
+
 @Schema()
 export class User extends Document {
-    @Prop({ required: true })
-    name: string;
+  @Prop({ required: true })
+  name: string;
 
-    @Prop({ required: true })
-    email: string;
+  @Prop({ required: true })
+  email: string;
 
-    @Prop({ required: true })
-    password: string;
+  @Prop({ required: true })
+  password: string;
 
-    @Prop({ required: true })
-    gender: string;
+  @Prop({ required: true })
+  gender: string;
 
-    @Prop({ required: true })
-    phone: number;
+  @Prop({ required: true , type: String,
+    enum: UserRole, default: UserRole.CLIENT,
+  })
+  role: string;
 }
 
 export const UserModelSchema = SchemaFactory.createForClass(User);
