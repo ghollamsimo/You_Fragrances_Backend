0426941efc933656a7e4359c854930fd2b7fb14d
ghollamsimo <ghollamsimo1@gmail.com>
2024-12-25 15:00:12 +0100

    Feat --> Adding field of gender with role as string and removing field of phone

--- src/core/entities/user.entity.ts
+++ src/core/entities/user.entity.ts
@@ -1,9 +1,9 @@
-export class User {
+export class UserEntity {
     constructor(
-        public readonly name: unknown,
+        public readonly name: string,
         public readonly email: string,
+        public readonly role: string,
         public password: string,
         public readonly gender: string,
-        public readonly phone: number
     ) {}
 }
