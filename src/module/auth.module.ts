a23fd06585624737b01d9e92e42c6b8d45be5932
ghollamsimo <ghollamsimo1@gmail.com>
2025-01-03 16:28:43 +0100

    Feat --> Adding folder of guards with file auth.guard and import the class of JwtAuthGuard in auth.module

--- src/module/auth.module.ts
+++ src/module/auth.module.ts
@@ -6,16 +6,19 @@ import { User, UserModelSchema } from '../infrastructure/db/schemas/user.schema'
 import { UserController } from '../interface/http/user.controller';
 import { UserUseCase } from '../application/usecases/user.usecase';
 import { UserRepositoryImpl } from '../infrastructure/repositories/user.repository.impl';
+import { JwtAuthGuard } from '../guard/guard';  
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserModelSchema }]),
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+        secret: configService.get<string>('JWT_SECRET'), 
         signOptions: { expiresIn: '1h' },
       }),
     }),
@@ -27,7 +30,9 @@ import { UserRepositoryImpl } from '../infrastructure/repositories/user.reposito
       provide: 'UserInterface',
       useClass: UserRepositoryImpl,
     },
-    UserRepositoryImpl, 
+    UserRepositoryImpl,
+    JwtAuthGuard, 
   ],
+  exports: [JwtModule], 
 })
 export class AuthModule {}
