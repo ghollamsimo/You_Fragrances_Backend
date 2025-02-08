59c6d6b6c69442f7b4bec8977d54e8ebbb20d7f0
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-09 00:47:43 +0100

    Feat --> Adding property of chatHistory and create a method fot see all history with Ai

--- src/application/usecases/chatbot.usecase.ts
+++ src/application/usecases/chatbot.usecase.ts
@@ -12,6 +12,7 @@ export class ChatbotUseCase  {
   private chatSessions: { [sessionId: string]: ChatSession } = {}
   private sessionCounters: { [sessionId: string]: number } = {} 
   private readonly logger = new Logger(ChatbotUseCase .name)
+  private globalChatHistory: { [sessionId: string]: string[] } = {};
 
   constructor(configService: ConfigService) {
     const geminiApiKey = configService.get('OPENAI_API_KEY');
@@ -28,6 +29,7 @@ export class ChatbotUseCase  {
       result = this.model.startChat();
       this.chatSessions[sessionIdToUse] = result;
       this.sessionCounters[sessionIdToUse] = 0; 
+      this.globalChatHistory[sessionIdToUse] = [];
     }
 
     return {
@@ -42,6 +44,8 @@ export class ChatbotUseCase  {
 
       this.sessionCounters[sessionId]++;
 
+      this.globalChatHistory[sessionId].push(`User: ${data.prompt}`);
+
       if (this.sessionCounters[sessionId] > 5) {
         return {
           result: "You've used this service more than 5 times. Please subscribe for continued access.",
@@ -52,12 +56,25 @@ export class ChatbotUseCase  {
       const prompt = `You are a perfume expert. ${data.prompt}`;
       const result = await chat.sendMessage(prompt);
 
+      const fullResponse = await result.response.text();
+
+      const summaryPrompt = `Please summarize the following response in one or two sentences: ${fullResponse}`;
+      const summaryResult = await chat.sendMessage(summaryPrompt);
+      const summary = await summaryResult.response.text();
+
+      this.globalChatHistory[sessionId].push(`AI : ${summary}`);
+
       return {
-        result: await result.response.text(),
+        result: summary, 
         sessionId
       };
     } catch (error) {
       this.logger.error("ERROR sending message to Gemini API for perfumes", error);
+      throw error;
     }
+  }
+
+  getGlobalChatHistory() {
+    return this.globalChatHistory;
   }
 }
