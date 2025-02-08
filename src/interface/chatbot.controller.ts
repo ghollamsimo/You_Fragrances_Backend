8e8c2763f72e44ab9ce66ffe37e32550c4d37bca
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-08 22:57:16 +0100

    Feat --> Adding file of ChatbotController in folder interfaces

---
+++ src/interface/chatbot.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, Body, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ChatbotUseCase } from '../application/usecases/chatbot.usecase';
+import { JwtAuthGuard } from 'src/guard/guard';
+import { GetAIMessageDTO } from 'src/core/dto/get-ai-response.dto';
+import { v4 as uuidv4 } from 'uuid'; 
+
+@Controller('chatbot')
+export class ChatbotController {
+  constructor(private readonly chatbotService: ChatbotUseCase) {}
+
+  @Post('message')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async getResponse(@Body() data: GetAIMessageDTO) {
+    const sessionId = data.sessionId || uuidv4();
+        return await this.chatbotService.generatePerfumeAdvice({ sessionId, prompt: data.prompt });
+  }
+}
