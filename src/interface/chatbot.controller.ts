066055e0f6b8ebd546812cf6cc591f2ac918dd3d
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-09 00:48:40 +0100

    Feat --> Adding method of getHistoryChats

--- src/interface/chatbot.controller.ts
+++ src/interface/chatbot.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, UsePipes, ValidationPipe, Get } from '@nestjs/common';
 import { ChatbotUseCase } from '../application/usecases/chatbot.usecase';
 import { JwtAuthGuard } from 'src/guard/guard';
 import { GetAIMessageDTO } from 'src/core/dto/get-ai-response.dto';
@@ -13,5 +13,10 @@ export class ChatbotController {
   async getResponse(@Body() data: GetAIMessageDTO) {
     const sessionId = data.sessionId || uuidv4();
         return await this.chatbotService.generatePerfumeAdvice({ sessionId, prompt: data.prompt });
+  }
+  @Get('history')
+  getHistoryChats(){
+    const history = this.chatbotService.getGlobalChatHistory()
+    return {history}
   }
 }
