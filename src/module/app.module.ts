ac9f57bf49bf9a4800ffd5354b8f7c601f7a822e
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-08 23:00:25 +0100

    Feat --> Importing module of chatBot

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -3,12 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth.module';
 import { BrandModule } from './brand.module';
+import { ChatbotModule } from './chatbot.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/youFragrances'),   
+    ConfigModule.forRoot({isGlobal: true}),
     AuthModule,
-    BrandModule
+    BrandModule,
+    ChatbotModule
   ],
   controllers: [],
   providers: [],
