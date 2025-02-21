82039ce28badaf8dd45acd3cd93356c2d8b61746
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-22 00:34:20 +0100

    Feat --> Adding file of perfume.controller in folder http

---
+++ src/interface/http/perfume.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { identity } from "rxjs";
+import { PerfumeUseCase } from "src/application/usecases/perfume.usecase";
+import { PerfumeDTO } from "src/core/dto/perfume.dto";
+
+@Controller('perfumes')
+export class PerfumeController{
+    constructor(private readonly perfumeUseCase: PerfumeUseCase){}
+
+    @Post('store')
+    @UseInterceptors(FileInterceptor('image'))
+    async store(@Body() perfumeDTO: PerfumeDTO, @UploadedFile() file: Express.Multer.File){
+        return await this.perfumeUseCase.store(perfumeDTO, file)
+    }
+
+    @Get('allPerfumes')
+    async index(){
+        return await this.perfumeUseCase.index()
+    }
+
+    @Get('index')
+    async getIndexScreen(){
+        return await this.perfumeUseCase.getIndexScreen()
+    }
+
+    @Delete('destroy/:id')
+    async delete(@Param('id') id: string){
+        return await this.perfumeUseCase.delete(id)
+    }
+
+    @Put('update/:id')
+    async update(@Param('id') id: string, perfumeDto: PerfumeDTO){
+        
+    }
+}
