6c19e9cc8dc8f9921c70cbd9bf1041de5b71ee09
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-21 14:50:31 +0100

    Feat --> remove old methods and defind new method with there logic

--- src/application/usecases/minio.usecase.ts
+++ src/application/usecases/minio.usecase.ts
@@ -1,39 +1,43 @@
-import { Injectable } from '@nestjs/common';
-import { Client } from 'minio';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as Minio from 'minio';
 
 @Injectable()
-export class MinioService {
-  private readonly minioClient: Client;
+export class MinioService implements OnModuleInit {
+  private minioClient: Minio.Client;
+  private readonly bucketName = 'fragrances';
 
   constructor(configService: ConfigService) {
-    this.minioClient = new Client({
-      endPoint: configService.get('MINIO_END_POINT'), 
-      port: configService.get('MINIO_PORT'),                     
-      useSSL: false,                  
-      accessKey: configService.get('MINIO_ACCESS_KEY'),
+    this.minioClient = new Minio.Client({
+      endPoint: configService.get('MINIO_END_POINT'),
+      port: configService.get('MINIO_PORT'),
+      useSSL: false,
+      accessKey:  configService.get('MINIO_ACCESS_KEY'),
       secretKey: configService.get('MINIO_SECRET_KEY'),
     });
   }
 
-  async uploadFile(bucketName: string, objectName: string, filePath: string): Promise<string> {
-    await this.minioClient.fPutObject(bucketName, objectName, filePath, {
-      'Content-Type': 'application/octet-stream',
-    });
-    return `File uploaded successfully to ${bucketName}/${objectName}`;
-  }
-
-  async getFileUrl(bucketName: string, objectName: string): Promise<string> {
-    return await this.minioClient.presignedGetObject(bucketName, objectName);
-  }
-
-  async createBucket(bucketName: string): Promise<void> {
-    const bucketExists = await this.minioClient.bucketExists(bucketName);
-    if (!bucketExists) {
-      await this.minioClient.makeBucket(bucketName, 'us-east-1');
-      console.log(`Bucket ${bucketName} created successfully`);
-    } else {
-      console.log(`Bucket ${bucketName} already exists`);
+  async onModuleInit() {
+    const exists = await this.minioClient.bucketExists(this.bucketName);
+    if (!exists) {
+      await this.minioClient.makeBucket(this.bucketName, '');
+      console.log(`Bucket "${this.bucketName}" created successfully.`);
     }
   }
+
+  async uploadFile(file: Express.Multer.File, folderName: string): Promise<string> {
+    try {
+      const objectName = `${folderName}/${Date.now()}-${file.originalname}`;
+  
+      await this.minioClient.putObject(this.bucketName, objectName, file.buffer, file.size, {
+        'Content-Type': file.mimetype,
+      });
+  
+      return `http://127.0.0.1:9000/${this.bucketName}/${objectName}`;
+    } catch (error) {
+      console.error('Minio Upload Error:', error);
+      throw new Error('Failed to upload file to Minio');
+    }
+  }
+  
 }
