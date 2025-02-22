2def6c88394a8f66cfc4f270ecc6e73a1e41415c
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-22 02:04:53 +0100

    Feat --> Implemet mehtod  of getindex with there logic

--- src/infrastructure/repositories/brand.repository.impl.ts
+++ src/infrastructure/repositories/brand.repository.impl.ts
@@ -1,20 +1,27 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-
 import { Brand as BrandDocument } from "../db/schemas/brand.schema";
 import { Model } from "mongoose";
 import { BrandInterface } from "src/core/interfaces/brand.interface";
 import { BrandEntity } from "src/core/entities/brand.entity";
 import { BrandDTO } from "src/core/dto/brand.dto";
+import { GetBrandIndexScreenDto } from "src/core/dto/get/brand-get-index.dto";
 
 @Injectable()
 export class BrandRepositoryImpl implements BrandInterface {
   constructor(@InjectModel(BrandDocument.name) private readonly brandModel: Model<BrandDocument>) {}
 
+  async getIndexScreen(): Promise<GetBrandIndexScreenDto[]> {
+    const brands = await this.brandModel.find({}, '_id name image description').lean()
+    return brands.map(brand => new GetBrandIndexScreenDto(String(brand._id), brand.name, brand.image, brand.description))
+  }
+
   async store(brandDTO: BrandDTO): Promise<BrandEntity> {
+    const existingBrand = await this.brandModel.findOne({ name: brandDTO.name }).exec();
+    if (existingBrand) throw new Error('Brand already exists');
     const createdBrand = new this.brandModel(brandDTO);
     const savedBrand = await createdBrand.save();
-    return new BrandEntity(savedBrand.name, savedBrand.image, savedBrand.description, savedBrand.type);
+    return new BrandEntity(savedBrand.name, savedBrand.image, savedBrand.description, savedBrand.country, savedBrand.founded);
   }
 
   async delete(id: string): Promise<{ message: string }> {
