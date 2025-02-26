64b06db9322c5a32ecb28d2ea7958cab4306b694
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-26 11:04:13 +0000

    Feat --> Adding Fields and adding in GetPerfumeIndexScreenDto proprity of AverageRating

--- src/core/dto/perfume.dto.ts
+++ src/core/dto/perfume.dto.ts
@@ -1,10 +1,12 @@
-
 export class PerfumeDTO {
-    name: string;
-    image: string;
-    brand: string;
-    topNotes: string[];
-    middleNotes: string[];
-    baseNotes: string[];
-  }
-  
+  name: string;
+  image: string;
+  brand: string;
+  topNotes: string[];
+  middleNotes: string[];
+  baseNotes: string[];
+  TargetAudience: string;
+  Volume: string;
+  Concentration: string;
+  sillage: string;
+}
