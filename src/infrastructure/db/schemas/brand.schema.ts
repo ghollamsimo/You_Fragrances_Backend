6d043a88d2577d7854a719fe9bb9114c76458191
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-26 11:05:04 +0000

    Feat --> Adding BrandDocument

--- src/infrastructure/db/schemas/brand.schema.ts
+++ src/infrastructure/db/schemas/brand.schema.ts
@@ -1,6 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+export type BrandDocument = Brand & Document;
+
 
 @Schema()
 export class Brand extends Document {
