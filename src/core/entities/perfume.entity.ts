ae20ce20c8cdf770b8e1fc05bd63ae1526d3792a
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-26 11:04:37 +0000

    Feat --> Adding new Fields

--- src/core/entities/perfume.entity.ts
+++ src/core/entities/perfume.entity.ts
@@ -1,16 +1,16 @@
-import {Schema as MongooseSchema } from 'mongoose';
+import { Schema as MongooseSchema } from 'mongoose';
 
 export class PerfumeEntity {
-
-    constructor(private readonly name: string,
+    constructor(
+        private readonly name: string,
         private readonly image: string,
         private readonly brand: string,
-        private readonly topNotes: MongooseSchema.Types.ObjectId
-,
-        private readonly middleNotes: MongooseSchema.Types.ObjectId
-,
-        private readonly baseNotes: MongooseSchema.Types.ObjectId
-) {
-    }
-  }
-  
+        private readonly topNotes: MongooseSchema.Types.ObjectId,
+        private readonly middleNotes: MongooseSchema.Types.ObjectId,
+        private readonly baseNotes: MongooseSchema.Types.ObjectId,
+        private readonly TargetAudience: string,
+        private readonly Volume: string,
+        private readonly Concentration: string,
+        private readonly sillage: string
+    ) {}
+}
